df1b06b011662cd3d68b3f3eea34c07341fabe0e
SQ1-SAC2 <126805421+SQ1-SAC2@users.noreply.github.com>
2023-03-07 18:06:20 -0400

    Large Cactus update

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -5,12 +5,11 @@ from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
 
 class Cactus(Obstacle):
     def __init__(self):
+        cactus_type = random.randint(0,2)
         if random.randint(0,1) == 0:
-            cactus_type = random.randint(0,2)
             image = SMALL_CACTUS[cactus_type]
             cactus_y = 325
         else:
-            cactus_type = random.randint(0,2)
             image = LARGE_CACTUS[cactus_type]
             cactus_y = 300
         
