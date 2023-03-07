a4336e41f65de89c58a12c855b728ae1f5104f84
SQ1-SAC2 <126805421+SQ1-SAC2@users.noreply.github.com>
2023-03-07 17:51:40 -0400

    Class 02: Large Cactus and Bird

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,11 +1,18 @@
 import random
 from dino_runner.components.obstacles.obstacle import Obstacle
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
 
 
 class Cactus(Obstacle):
     def __init__(self):
-        cactus_type = random.randint(0, 2)
-        image = SMALL_CACTUS[cactus_type]
+        if random.randint(0,1) == 0:
+            cactus_type = random.randint(0,2)
+            image = SMALL_CACTUS[cactus_type]
+            cactus_y = 325
+        else:
+            cactus_type = random.randint(0,2)
+            image = LARGE_CACTUS[cactus_type]
+            cactus_y = 300
+        
         super().__init__(image)
-        self.rect.y = 325
+        self.rect.y = cactus_y
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,4 +1,6 @@
+import random
 import pygame
+from dino_runner.components.obstacles.bird import Bird
 from dino_runner.components.obstacles.cactus import Cactus
 
 
@@ -6,13 +8,16 @@ class ObstacleManager:
     def __init__(self):
         self.obstacles = []
 
-    def update(self, game):
+    def update(self, game_speed, player, game):
         if not self.obstacles:
-            self.obstacles.append(Cactus())
+            if random.randint(0, 2) == 2:
+                self.obstacles.append(Bird())
+            else:
+                self.obstacles.append(Cactus())
 
         for obstacle in self.obstacles:
-            obstacle.update(game.game_speed, self.obstacles)
-            if game.rect.colliderect(obstacle.rect):
+            obstacle.update(game_speed, self.obstacles)
+            if player.rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
 
